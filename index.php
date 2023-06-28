7ca2ca27e4b0ffd924632905ba044ac387b28d1a
Nicolas Perez <nicoperezre@gmail.com>
2023-06-28 17:44:01 -0400

    First commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+	<head>
+	    <meta charset="utf-8">
+	    <title>Getin UX</title>
+	    <meta name="description" content="">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	    <link rel="shortcut icon" href="assets/img/favicon.png"/>
+	</head>
+
+	<body>
+		<ul>
+			<li>
+				<a href="v-home.php">
+					Home
+				</a>
+			</li>
+			<li>
+				<a href="v-nosotros.php">
+					Nosotros
+				</a>
+			</li>
+			<li>
+				<a href="v-contacto.php">
+					Contacto
+				</a>
+			</li>
+		</ul>
+	</body>
+
+</html>
---
+++ v-contacto.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="title" content="Get in UX"/>
+    <meta name="description" content="Encuentra el programa que impulsará tu carrera al siguiente nivel"/>
+    <meta property="og:title" content="Get in UX"/>
+    <meta property="og:description" content="Encuentra el programa que impulsará tu carrera al siguiente nivel"/>
+
+    <meta property="og:type" content="company"/>
+    <meta property="og:url" content="https://www.getinuxcl.com"/>
+    <meta property="og:site_name" content="Get in UX"/>
+    <title>Get in UX</title>
+    <link rel="shortcut icon" href="dist/img/favicon.png"/>
+    <link rel="stylesheet" href="dist/css/style.css?cb=1615307255841">
+</head>
+
+<body class="transition">
+    <div class="sidebar">
+        <div class="sidebar__layer"></div>
+        <div class="sidebar__box">
+            <a href="" class="sidebar__close sidebar__trigger"></a>
+            <ul>
+                <li>
+                    <a href="v-home.php">Inicio</a>
+                </li>
+                <li>
+                    <a href="v-nosotros.php">Sobre el sitio</a>
+                </li>
+                <li>
+                    <a href="v-contacto.php">Incluye tu programa</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    
+    <div class="header transition">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="header__menu">
+                        <a href="#" class="header__switch sidebar__trigger"></a>
+                        <a href="#" class="header__contrast"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="bgblock">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h3 class="u-center text text-5xl text--bold text-darkblue u-mb10">
+                        Incluye tu <span>programa</span>
+                    </h3>
+                    <h4 class="u-center text text-base text-darkblue text--medium u-mb0">
+                        Sé parte de Get in UX
+                    </h4>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="u-pt30 u-pb30">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 u-mb40">
+                    <p class="text text-sm u-mb20">
+                        ¿Quieres que tu programa de enseñanza en UX sea visto por miles de potenciales estudiantes? Pues has llegado a la sección correcta.
+                    </p>
+                    <p class="text text-sm u-mb20">
+                        Para ser parte de nuestro sitio web tienes que rellenar el siguiente <a href="https://forms.gle/9pMCuhv3LMDPyKPj8" target="_blank">formulario de inscripción</a>. 
+                    </p>
+                    <p class="text text-sm u-mb20">
+                        Los programas que buscamos tienen que ver con áreas relacionadas a la UX, tales como UX, UI, CX, Product Design, Content Design, UX Research, Diseño Web, entre otras.
+                    </p>
+                    <p class="text text-sm u-mb20">
+                        Responderemos tu solicitud entre 48 y 72 horas aproximadamente, dependiendo de la demanda que tengamos.
+                    </p>
+                    <p class="text text-sm">
+                        Si todo está correcto, ¡te agregaremos!
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="footer transition">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <a href="#" class="footer__title text-xl text u-mb0 text--medium">
+                        Get in <br>
+                        UX
+                    </a>
+                </div>
+                <div class="col-6">
+                    <ul>
+                        <li>
+                            <a href="" class="text text-sm">
+                                Sobre el sitio
+                            </a>
+                        </li>
+                        <li>
+                            <a href="" class="text text-sm">
+                                Incluye tu programa
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-12">
+                    <p class="text text-xxs u-center u-mb0">
+                        Creado por humanos. Get in UX Chile 2023. <br>
+                        ©Todos los derechos reservados.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="dist/js/all.js?cb=1615307255841"></script>
+</body>
+
+</html>
