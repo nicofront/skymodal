5ad75997b7e1d1c5b875d98090bc24f8164f72d5
Nicolas Perez <nicoperezre@gmail.com>
2023-08-25 12:31:49 -0400

    update javascript

--- dev/js/main.js
+++ dev/js/main.js
@@ -2,27 +2,20 @@ $(document).ready(function() {
 
   let prevUrl = '';
   let inactivitySwitch = 1;
-  let inactivityLoop = 8000;
-  inactivityLoop = 300;
+  let inactivityLoop = 5000;
+  // inactivityLoop = 300;
 
-  var target_date = new Date().getTime() + (1000*60*5); // set the countdown date
+  var target_date = 0; // set the countdown date
   var days, hours, minutes, seconds; // variables for time units
 
-  var countdown = document.getElementById("tiles"); // get tag element
-
-  getCountdown();
-
-  setInterval(function () { getCountdown(); }, 1000);
-
   function getCountdown(){
-
     // find the amount of "seconds" between now and target
     var current_date = new Date().getTime();
     var seconds_left = (target_date - current_date) / 1000;
 
     if(seconds_left <= 0) {
       seconds_left = 0;
-      window.location.href = "http://www.w3schools.com";
+      window.location.href = "https://www.skyairline.com/chile";
     }
 
     days = pad( parseInt(seconds_left / 86400) );
@@ -34,57 +27,58 @@ $(document).ready(function() {
     minutes = pad( parseInt(seconds_left / 60) );
     seconds = pad( parseInt( seconds_left % 60 ) );
 
+    var countdown = document.getElementById("tiles");
     // format countdown string + set tag value
-    countdown.innerHTML = "<span>" + hours + ":</span><span>" + minutes + ":</span><span>" + seconds + "</span>"; 
+    if(countdown){
+      countdown.innerHTML = "<span>" + hours + ":</span><span>" + minutes + ":</span><span>" + seconds + "</span>"; 
+    }
   }
 
   function pad(n) {
     return (n < 10 ? '0' : '') + n;
   }
 
-
-
   var inactivityTime = function () {
-      var time;
-      window.onload = resetTimer;
-      // DOM Events
-      document.addEventListener('onmousemove', resetTimer, true);
-      document.addEventListener('onmousedown', resetTimer, true);
+    var time;
+    window.onload = resetTimer;
+    // DOM Events
+    document.addEventListener('onmousemove', resetTimer, true);
+    document.addEventListener('onmousedown', resetTimer, true);
 
-      document.addEventListener('onkeydown', resetTimer, true);
+    document.addEventListener('onkeydown', resetTimer, true);
 
-      document.addEventListener('ontouchstart', resetTimer, true);
-      document.addEventListener('ontouchend', resetTimer, true);
-      document.addEventListener('ontouchcancel', resetTimer, true);
+    document.addEventListener('ontouchstart', resetTimer, true);
+    document.addEventListener('ontouchend', resetTimer, true);
+    document.addEventListener('ontouchcancel', resetTimer, true);
 
+    document.addEventListener('click', resetTimer, true);
+    document.addEventListener('scroll', resetTimer, true);
 
-      document.addEventListener('click', resetTimer, true);
-      document.addEventListener('scroll', resetTimer, true);
-
-      let form = document.querySelector('form');
-      form.addEventListener('input', function (event) {
-        console.log('form');
-        inactivityLoop = 12000;
+    let form = document.querySelector('form');
+    form.addEventListener('input', function (event) {
+      console.log('form');
+      inactivityLoop = 15000;
+    });
+    let boxes = Array.from(document.getElementsByClassName('el-select-dropdown__item'));
+    boxes.forEach(box => {
+      box.addEventListener('click', function handleClick(event) {
+        console.log('select');
+        inactivityLoop = 15000;
       });
-      let boxes = Array.from(document.getElementsByClassName('el-select-dropdown__item'));
-      boxes.forEach(box => {
-        box.addEventListener('click', function handleClick(event) {
-          console.log('select');
-          inactivityLoop = 12000;
-        });
-      });
+    });
 
-      function modalTrigger() {
-          if(inactivitySwitch){
-            inactivitySwitch = 0;
-            console.log(inactivityLoop);
-            showModal();
-          }
+    function modalTrigger() {
+      if(inactivitySwitch){
+        inactivitySwitch = 0;
+        target_date = new Date().getTime() + (1000*60*5);
+        console.log(inactivityLoop);
+        showModal();
       }
-      function resetTimer() {
-          clearTimeout(time);
-          time = setTimeout(modalTrigger, inactivityLoop)
-      }
+    }
+    function resetTimer() {
+      clearTimeout(time);
+      time = setTimeout(modalTrigger, inactivityLoop)
+    }
   };
 
   const modalInterval = setInterval(modalTimer, 10);
@@ -113,8 +107,7 @@ $(document).ready(function() {
 
   function showModal () {
 
-   const url = getCurrentURL();
-   console.log(url);
+   setInterval(function () { getCountdown(); }, 1000);
 
    document.addEventListener('click', function (event) {
      const elem = document.getElementById("modal-reserva");
@@ -126,7 +119,6 @@ $(document).ready(function() {
    setTimeout(() => {
     var modalReserva = document.getElementById("modal-reserva");
     modalReserva.classList.add("active");
-
    }, 10);
 
   } // end showModal
